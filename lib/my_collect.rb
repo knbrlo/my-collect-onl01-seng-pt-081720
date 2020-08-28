92959f76ae20e5e0fb441667307f299f23991e64
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 01:59:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(array)
   count = 0
+  collection = []
   while count < array.length
-    yield(array[count])
+    collection << yield(array[count])
     count += 1
   end
+  collection
 end
 
 my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |language|
