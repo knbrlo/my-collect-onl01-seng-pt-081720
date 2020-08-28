9036d493ee3a95f289d9144ce1086d4a1dab1224
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 01:58:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,10 @@
-def my_collect(array) do |language|
+def my_collect(array)
+  count = 0
+  while count < array.length
+    yield(array[count])
+  end
+end
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |language|
   language.upcase
 end
