eb6ffcfd9cd399dee5f6f1228a4a8da6dee17bed
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 01:58:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,10 @@ def my_collect(array)
   count = 0
   while count < array.length
     yield(array[count])
+    count += 1
   end
 end
 
 my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |language|
-  language.upcase
+  puts language.upcase
 end
